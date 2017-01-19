d451c28539b7901000d1af3dda565d2722c7103f
Murillo Vasconcelos <murillobittencourt@gmail.com>
2017-01-19 18:57:28 -0200

    Use strict comparison to check selected values when building select options

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -697,7 +697,7 @@ class FormBuilder
           return in_array($value, $selected) ? 'selected' : null;
       }
 
-      return ((string) $value == (string) $selected) ? 'selected' : null;
+      return ((string) $value === (string) $selected) ? 'selected' : null;
   }
 
   /**
